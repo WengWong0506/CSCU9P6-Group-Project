fc70f00f386dc5a8f80726263aa1313811fd14c6
Martin Petrov <marti21248@gmail.com>
2022-03-14 21:23:57 +0200

    GUI update
    Implementation of the basic GUI of 4 of the Observer classes: LATC, MaintenanceInspector, RadarTransceiver, RefuellingSupervisor

--- src/Driver/Main.java
+++ src/Driver/Main.java
@@ -5,6 +5,13 @@ Ensure it is only your Student ID and *not* your name as marking is done anonymo
 Please only add your name on this class if you have worked on this class.
 Work can take any form from refactoring to code writing and anything in between, of course
 You should always take credit for your work.*/
+
+import Management.AircraftManagementDatabase;
+import Observers.LATC;
+import Observers.MaintenanceInspector;
+import Observers.RadarTransceiver;
+import Observers.RefuellingSupervisor;
+
 /**
  * @author 2819600
  * @author 
@@ -18,6 +25,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		//TODO Auto-Generated Method Stub
+		AircraftManagementDatabase model1 = new AircraftManagementDatabase();
+		RadarTransceiver radar = new RadarTransceiver(model1, "Radar Transceiver");
+		LATC latc = new LATC(model1, "Radar Transceiver");
+		MaintenanceInspector mainInspector = new MaintenanceInspector(model1, "Radar Transceiver");
+		RefuellingSupervisor refuellingSupervisor = new RefuellingSupervisor(model1,"Refuelling Supervisor");
 	}
 	
 	
